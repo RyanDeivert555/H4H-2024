e82c2d0917d4d6fa9c7f5909de4eca9cfeb20e9e
i-r-e-n-e <85856587+i-r-e-n-e@users.noreply.github.com>
2024-02-17 18:36:54 -0800

    datapipes

--- chart.ts
+++ chart.ts
@@ -1,9 +1,26 @@
 import Chart from 'chart.js/auto';
-import { getFirestore,getDoc,doc } from 'firebase/firestore';
-import { initializeApp } from 'firebase/app';
+import { getFirestore, getDoc, doc, getDocs, collection, addDoc, setDoc, deleteDoc, DocumentData } from "firebase/firestore";
+import { initializeApp } from "firebase/app";
 
+const firebaseConfig = {
+    apiKey: "AIzaSyBZ-Mqa6xxDKLxK3vxHEfdjPaaAzXMoLDs",
+    authDomain: "test-project-df70f.firebaseapp.com",
+    projectId: "test-project-df70f",
+    storageBucket: "test-project-df70f.appspot.com",
+    messagingSenderId: "124174617130",
+    appId: "1:124174617130:web:c14a4ad8ce25ce6e7a9153",
+    measurementId: "G-R5CJCVW322"
+};
 
+const app = initializeApp(firebaseConfig)
 
+const reference = doc(getFirestore(), "reviews", "base");
+const docSnap = await getDoc(reference);
+const taste = docSnap.get("taste");
+const location = docSnap.get("location");
+const accessibility = docSnap.get("accessibility");
+const health = docSnap.get("health");
+const pressure = docSnap.get("pressure");
 
 const ctx = <HTMLCanvasElement>document.getElementById('myChart');
 
@@ -13,8 +30,8 @@ const chart = new Chart(ctx, {
       labels: ['Taste', 'Location', 'Accesibility', 'Cleanliness', 'Water Pressure'],
       datasets: [{
         label: 'Drinking Fountain Rating',
-        data: [1.0, 3.5, 2.7, 4.2, 5],
-        //data: [taste.value, location.value, accessibility.value, health.value, temperature.value];
+        //data: [1.0, 3.5, 2.7, 4.2, 5],
+        data: [taste, location, accessibility, health, pressure],
         borderWidth: 1,
         fill: true,
         backgroundColor: 'rgba(255, 99, 132, 0.2)',
