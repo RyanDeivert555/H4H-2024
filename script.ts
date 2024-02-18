9e19deb12a1cf356ac367f578ffe8c1363f20f6c
Ryan Deivert <rdeivert@icloud.com>
2024-02-17 20:08:13 -0800

    cleaned up code

--- script.ts
+++ script.ts
@@ -1,18 +1,5 @@
-import { getFirestore, collection, addDoc } from "firebase/firestore";
-import { initializeApp } from "firebase/app";
-
-const firebaseConfig = {
-    apiKey: "AIzaSyBZ-Mqa6xxDKLxK3vxHEfdjPaaAzXMoLDs",
-    authDomain: "test-project-df70f.firebaseapp.com",
-    projectId: "test-project-df70f",
-    storageBucket: "test-project-df70f.appspot.com",
-    messagingSenderId: "124174617130",
-    appId: "1:124174617130:web:c14a4ad8ce25ce6e7a9153",
-    measurementId: "G-R5CJCVW322"
-};
-
-const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
+import { collection, addDoc } from "firebase/firestore";
+import db from "./config.ts";
 
 type Data = {
     taste: number,
