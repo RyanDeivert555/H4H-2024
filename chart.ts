6c0370fd8eecab20a27554b5caf1c354aa55473c
i-r-e-n-e <85856587+i-r-e-n-e@users.noreply.github.com>
2024-02-17 18:02:47 -0800

    update

--- chart.ts
+++ chart.ts
@@ -1,14 +1,20 @@
 import Chart from 'chart.js/auto';
+import { getFirestore,getDoc,doc } from 'firebase/firestore';
+import { initializeApp } from 'firebase/app';
 
-const ctx = document.getElementById('myChart');
+
+
+
+const ctx = <HTMLCanvasElement>document.getElementById('myChart');
 
 const chart = new Chart(ctx, {
     type: 'radar',
     data: {
       labels: ['Taste', 'Location', 'Accesibility', 'Cleanliness', 'Water Pressure'],
       datasets: [{
-        label: 'Water Rating',
+        label: 'Drinking Fountain Rating',
         data: [1.0, 3.5, 2.7, 4.2, 5],
+        //data: [taste.value, location.value, accessibility.value, health.value, temperature.value];
         borderWidth: 1,
         fill: true,
         backgroundColor: 'rgba(255, 99, 132, 0.2)',
