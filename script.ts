50b20c34dfba4567156e5860949adc1f14d8a269
Ryan Deivert <rdeivert@icloud.com>
2024-02-17 19:56:09 -0800

    removed unused imports

--- script.ts
+++ script.ts
@@ -1,4 +1,4 @@
-import { getFirestore, getDoc, doc, getDocs, collection, addDoc, setDoc, deleteDoc } from "firebase/firestore";
+import { getFirestore, collection, addDoc } from "firebase/firestore";
 import { initializeApp } from "firebase/app";
 
 const firebaseConfig = {
